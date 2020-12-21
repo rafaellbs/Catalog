a36553d43263fd22bc2ac461baddf7cf80d8e91d
rafaellbs <rafael_lbs@hotmail.com>
2020-12-21 08:21:31 -0300

    Category and Product

---
+++ backend/src/main/java/com/rafael/catalog/repositories/ProductRepository.java
@@ -0,0 +1,12 @@
+package com.rafael.catalog.repositories;
+
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.stereotype.Repository;
+
+import com.rafael.catalog.entities.Product;
+
+@Repository
+public interface ProductRepository extends JpaRepository<Product, Long>{
+
+	
+}
